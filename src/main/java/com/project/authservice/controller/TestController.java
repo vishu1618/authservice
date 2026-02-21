827ee606065f0d67520d286a23ae89d7bff7ed7a
Vishu Choudhary <226548324+vishu1618@users.noreply.github.com>
2026-02-21 14:45:37 +0530

    Initial Spring Boot setup with PostgreSQL and basic controller

---
+++ src/main/java/com/project/authservice/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.project.authservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Auth Service is running";
+    }
+}
